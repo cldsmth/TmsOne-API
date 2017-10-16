84352634af19fc57c9dd44a3c6b678d79f1104a2
Cliff Dean Smith <dean11.cliff@gmail.com>
2017-10-16 22:49:20 +0700

    adjust city with table mst_subarea

--- class/v1.0/City.php
+++ class/v1.0/City.php
@@ -1,14 +1,13 @@
 <?php
 class City{
 
-	private $table = "t_city";
-    private $joinJenis = "LEFT JOIN t_jenis jenis ON jenis.id_jenis = city.id_jenis";
+	private $table = "mst_subarea";
 
     public function get_list_by_province($province){
         $result = 0;
        
-        $text = "SELECT city.id_kab, city.nama, jenis.nama AS jenis 
-            FROM $this->table city $this->joinJenis WHERE city.id_prov = '$province'";
+        $text = "SELECT id_subarea, nama_subarea FROM $this->table 
+            WHERE id_area = '$province' AND status = 1";
         $query = mysql_query($text);
         if(mysql_num_rows($query) >= 1){
             $result = array();
--- class/v1.0/Province.php
+++ class/v1.0/Province.php
@@ -6,7 +6,8 @@ class Province{
     public function get_list(){
         $result = 0;
        
-        $text = "SELECT id_area, nama_area FROM $this->table WHERE status = 1 ORDER BY nama_area ASC";
+        $text = "SELECT id_area, nama_area FROM $this->table 
+            WHERE status = 1 ORDER BY nama_area ASC";
         $query = mysql_query($text);
         if(mysql_num_rows($query) >= 1){
             $result = array();
