b337196c91a183e7aace4c05e811281bbd83be42
Cliff Dean Smith <dean11.cliff@gmail.com>
2017-10-16 22:39:19 +0700

    adjust province with table mst_area

--- class/v1.0/Province.php
+++ class/v1.0/Province.php
@@ -1,12 +1,12 @@
 <?php
 class Province{
 
-	private $table = "t_province";
+	private $table = "mst_area";
 
     public function get_list(){
         $result = 0;
        
-        $text = "SELECT id_prov, nama FROM $this->table";
+        $text = "SELECT id_area, nama_area FROM $this->table WHERE status = 1 ORDER BY nama_area ASC";
         $query = mysql_query($text);
         if(mysql_num_rows($query) >= 1){
             $result = array();
