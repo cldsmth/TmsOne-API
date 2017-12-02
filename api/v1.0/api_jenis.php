2d8612969e0cb2e751e940bd1e1b4b08c4deae75
Cliff Dean Smith <dean11.cliff@gmail.com>
2017-12-03 01:23:10 +0700

    create action api jenis sync

--- api/v1.0/api_jenis.php
+++ api/v1.0/api_jenis.php
@@ -28,6 +28,28 @@ if(isset($_GET['action'])){
 		echo json_encode($R_message);	
 	}//end get jenis
 
+	//===================================== get sync ========================================
+	//start sync
+	else if($_GET['action'] == 'sync' && isset($_REQUEST['last_updated'])){
+		$obj_connect->up();	
+		$R_message = array("status" => "400", "message" => "No Data");
+
+		$N_last_updated = mysql_real_escape_string($_REQUEST['last_updated']);
+
+		$long_local = strtotime($N_last_updated) * 1000;
+		$long_server = strtotime($obj_jenis->get_last_updated()) * 1000;
+		if($long_server > $long_local){
+			$result = $obj_jenis->get_jenis_sync($N_last_updated);
+			//var_dump($result);
+			if(is_array($result)){
+				$R_message = array("status" => "200", "message" => "Data Exist", "data" => $result);
+			}
+		}
+
+		$obj_connect->down();	
+		echo json_encode($R_message);
+	}//end sync
+
 	else{
 		$R_message = array("status" => "404", "message" => "Action Not Found");
 		echo json_encode($R_message);
--- class/v1.0/Jenis.php
+++ class/v1.0/Jenis.php
@@ -3,6 +3,35 @@ class Jenis{
 
 	private $table = "jenis_produk";
 
+    public function get_jenis_sync($last_updated){
+        $result = 0;
+        $cond = $last_updated != "" ? "WHERE (UNIX_TIMESTAMP(timestamp) * 1000) > 
+            (UNIX_TIMESTAMP('$last_updated') * 1000)" : "";
+       
+        $text = "SELECT id_jenis, nama_jenis, keterangan, image_thumbnail, timestamp FROM $this->table $cond";
+        $query = mysql_query($text);
+        if(mysql_num_rows($query) >= 1){
+            $result = array();
+            while($row = mysql_fetch_assoc($query)){
+                $result[] = $row;
+            }
+        }
+        //$result = $text;
+        return $result;
+    }
+
+    public function get_last_updated(){
+        $result = 0;
+
+        $text = "SELECT MAX(timestamp) AS last_updated FROM $this->table";
+        $query = mysql_query($text);
+        if(mysql_num_rows($query) >= 1){
+            $row = mysql_fetch_assoc($query);
+            $result = $row['last_updated'];
+        }
+        return $result;
+    }
+
     public function get_list(){
         $result = 0;
        
